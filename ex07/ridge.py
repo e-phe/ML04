55f123a86104af82e3b2441d036e3a6d48bf02ae
ephe <ephe@student.42.fr>
2022-05-17 11:55:50 +0200

    feat-ex07-* : done

---
+++ ex07/ridge.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pprint
+
+
+def check_matrix(matrix):
+    if isinstance(matrix, np.ndarray) and matrix.size != 0 and len(matrix.shape) == 2:
+        return True
+    exit("Error matrix")
+
+
+def check_vector(vector):
+    if (
+        isinstance(vector, np.ndarray)
+        and vector.size != 0
+        and len(vector.shape) == 2
+        and vector.shape[1] == 1
+    ):
+        return True
+    exit("Error vector")
+
+
+class MyRidge:
+    """
+    Description:
+    My personal ridge regression class to fit like a boss.
+    """
+
+    def __init__(self, thetas, alpha=0.001, max_iter=1000, lambda_=0.5):
+        if (
+            check_vector(thetas)
+            and isinstance(alpha, float)
+            and isinstance(max_iter, int)
+            and isinstance(lambda_, float)
+        ):
+            self.alpha = alpha
+            self.max_iter = max_iter
+            self.thetas = thetas
+            self.lambda_ = lambda_
+        else:
+            exit("Error")
+
+    def get_params_(self):
+        return self.__dict__
+
+    def set_params_(self, **kwargs):
+        self.__dict__.update(kwargs)
+        return type(self).__name__ + pprint.pformat(self.__dict__)
+
+    def l2(self):
+        if check_vector(self.thetas):
+            ret = self.thetas[1:].T @ self.thetas[1:]
+            return ret[0][0]
+
+    def mse_(self, y, y_hat):
+        y = self.predict_(y)
+        if (
+            check_vector(y)
+            and check_vector(y_hat)
+            and y.shape == y_hat.shape
+            and isinstance(self.lambda_, float)
+        ):
+            return (((y_hat - y).T @ (y_hat - y))[0][0] + self.lambda_ * self.l2()) / (
+                2 * y.shape[0]
+            )
+
+    def loss_elem_(self, y, y_hat):
+        if check_vector(y) and check_vector(y_hat) and y.shape == y_hat.shape:
+            return np.array(
+                [(y_hat[i] - y[i]) * (y_hat[i] - y[i]) for i in range(y.shape[0])]
+            )
+        return
+
+    def predict_(self, x):
+        if isinstance(x, np.ndarray) and x.size != 0:
+            x = np.insert(x, 0, values=1.0, axis=1).astype(float)
+            return x @ self.thetas
+        return
+
+    def gradient_(self, x, y):
+        if (
+            check_vector(y)
+            and check_matrix(x)
+            and y.shape[0] == x.shape[0]
+            and check_vector(self.thetas)
+            and self.thetas.shape[0] == x.shape[1] + 1
+            and isinstance(self.lambda_, float)
+        ):
+            x = np.insert(x, 0, values=1.0, axis=1).astype(float)
+            theta_prime = np.array(self.thetas, copy=True)
+            theta_prime[0] = 0
+            return (x.T @ (x @ self.thetas - y) + self.lambda_ * theta_prime) / x.shape[
+                0
+            ]
+
+    def fit_(self, x, y):
+        if (
+            check_vector(y)
+            and check_matrix(x)
+            and y.shape[0] == x.shape[0]
+            and check_vector(self.thetas)
+            and self.thetas.shape[0] == x.shape[1] + 1
+            and isinstance(self.lambda_, float)
+        ):
+            x = np.insert(x, 0, values=1.0, axis=1).astype(float)
+            theta_prime = np.array(self.thetas, copy=True)
+            theta_prime[0] = 0
+            for _ in range(self.max_iter):
+                gradient = (
+                    x.T @ (x @ self.thetas - y) + self.lambda_ * theta_prime
+                ) / x.shape[0]
+                self.thetas = self.thetas - self.alpha * gradient
+            return self.thetas
+
+
+from sklearn.linear_model import Ridge
+
+if __name__ == "__main__":
+    y = np.array([[2], [14], [-13], [5], [12], [4], [-19]])
+    y_hat = np.array([[3], [13], [-11.5], [5], [11], [5], [-20]])
+    theta = np.array([[1], [2.5], [1.5], [-0.9]])
+    mr = MyRidge(theta)
+    rr = Ridge(theta)
+
+    print("get_params")
+    print(mr.get_params_())
+
+    print("\nset_params")
+    print(mr.set_params_(max_iter=1000))
+    print(rr.set_params(max_iter=1000))
+
+    print("\nloss")
+
+    print(mr.loss_(y, y_hat))
+    # Output: 0.8503571428571429
+
+    mr.lambda_ = 0.05
+    print(mr.loss_(y, y_hat))
+    # Output: 0.5511071428571429
+
+    mr.lambda_ = 0.9
+    print(mr.loss_(y, y_hat))
+    # Output: 1.116357142857143
+
+    print("\nloss_elem_")
+    print(mr.loss_elem_(y, y_hat))
+
+    x = np.array(
+        [
+            [-6, -7, -9],
+            [13, -2, 14],
+            [-7, 14, -1],
+            [-8, -4, 6],
+            [-5, -9, 6],
+            [1, -5, 11],
+            [9, -11, 8],
+        ]
+    )
+    y = np.array([[2], [14], [-13], [5], [12], [4], [-19]])
+    mr.thetas = np.array([[7.01], [3], [10.5], [-6]])
+
+    print("\npredict")
+    print(mr.predict_(x))
+
+    print("\ngradient")
+
+    mr.lambda_ = 1.0
+    print(mr.gradient_(x, y))
+    # Output: np.array([[-60.99], [-195.64714286], [863.46571429], [-644.52142857]])
+
+    mr.lambda_ = 0.5
+    print(mr.gradient_(x, y))
+    # Output: np.array([[-60.99], [-195.86142857], [862.71571429], [-644.09285714]])
+
+    mr.lambda_ = 0.0
+    print(mr.gradient_(x, y))
+    # Output: np.array([[-60.99], [-196.07571429], [861.96571429], [-643.66428571]])
+
+    print("\nfit")
+    print(mr.fit_(x, y))
---
+++ ex07/space_avocado.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+
+from ridge import MyRidge
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+def check_matrix(matrix):
+    if isinstance(matrix, np.ndarray) and matrix.size != 0 and len(matrix.shape) == 2:
+        return True
+    exit("Error matrix")
+
+
+def check_vector(vector):
+    if (
+        isinstance(vector, np.ndarray)
+        and vector.size != 0
+        and len(vector.shape) == 2
+        and vector.shape[1] == 1
+    ):
+        return True
+    exit("Error vector")
+
+
+def data_spliter(x, y, proportion):
+    if (
+        check_matrix(x)
+        and check_vector(y)
+        and x.shape[0] == y.shape[0]
+        and isinstance(proportion, float)
+        and proportion <= 1
+    ):
+        np.random.shuffle(x)
+        np.random.shuffle(y)
+        x_train, x_test = np.split(x, [int(proportion * x.shape[0])])
+        y_train, y_test = np.split(y, [int(proportion * y.shape[0])])
+        return (x_train, x_test, y_train, y_test)
+    return
+
+
+def add_polynomial_features(x, power):
+    if check_vector(x) and isinstance(power, int):
+        return np.vander(x.reshape(1, -1)[0], power + 1, increasing=True)[:, 1:]
+    return
+
+
+def vander_matrix(x, y):
+    x_ = np.zeros(y.shape)
+    for x_train in x.T:
+        x_ = np.concatenate(
+            (x_, add_polynomial_features(x_train.reshape(-1, 1), i)), axis=1
+        )
+    return x_[:, 1:]
+
+
+if __name__ == "__main__":
+    try:
+        if (
+            os.stat("../resources/space_avocado.csv").st_size > 0
+            and os.stat("models.csv").st_size > 0
+        ):
+            data = np.loadtxt(
+                "../resources/space_avocado.csv",
+                dtype=float,
+                delimiter=",",
+                skiprows=1,
+            )
+            models = np.genfromtxt("models.csv", dtype=str, delimiter="\t")
+        else:
+            exit("FileNotFoundError")
+    except:
+        exit("FileNotFoundError")
+
+    (x, x_test, y, y_test) = data_spliter(data[:, 1:4], data[:, [4]], 0.7)
+
+    theta = []
+    for el in models:
+        theta.append(np.fromstring(el, sep=","))
+    models = theta
+
+    figure, axis = plt.subplots(3, 4)
+    mse_train = np.zeros((4, 6))
+    mse_test = np.zeros((4, 6))
+    count = 0
+    for i in range(1, 5):
+        for j in range(6):
+            x_ = vander_matrix(x, y)
+            my_lr = MyRidge(theta[j + 1 + count].reshape(-1, 1), 1e-7, 10000, j / 5)
+
+            if i == 1:
+                my_lr = MyRidge(
+                    np.ones(x_.shape[1] + 1).reshape(-1, 1), 1e-7, 10000, j / 5
+                )
+                my_lr.fit_(x_, y)
+
+            if j == 0:
+                y_hat = np.empty(y.shape + (6,))
+            y_hat[:, :, j] = my_lr.predict_(x_)
+
+            x_test_ = vander_matrix(x_test, y_test)
+            mse_train[i - 1, j] = my_lr.mse_(x_, y)
+            mse_test[i - 1, j] = my_lr.mse_(x_test_, y_test)
+        count += 6
+        x_name = [
+            "weight(in ton)",
+            "prod_distance (in Mkm)",
+            "time_delivery (in days)",
+        ]
+        for j in range(x.shape[1]):
+            axis[j, i - 1].set_xlabel(x_name[j])
+            axis[j, i - 1].set_ylabel("target (in trantorian unit)")
+            axis[j, i - 1].set_ylim([1e5, 1e6])
+
+            axis[j, i - 1].scatter(x[:, j], y, label="dataset_train")
+            for k in range(6):
+                axis[j, i - 1].scatter(
+                    x[:, j],
+                    y_hat[:, :, k],
+                    marker=".",
+                    label="prediction lambda {}".format(k / 5),
+                )
+            axis[j, i - 1].legend()
+    plt.show()
+
+    plt.xlabel("polynomial degree")
+    plt.ylabel("mse")
+    for j in range(6):
+        plt.plot(
+            np.arange(1, 5), mse_train[:, j], label="mse_train lambda {}".format(j / 5)
+        )
+        plt.scatter(
+            np.arange(1, 5), mse_train[:, j], label="mse_train lambda {}".format(j / 5)
+        )
+        plt.plot(
+            np.arange(1, 5), mse_test[:, j], label="mse_test lambda {}".format(j / 5)
+        )
+        plt.scatter(
+            np.arange(1, 5),
+            mse_test[:, j],
+            marker=".",
+            label="mse_test lambda {}".format(j / 5),
+        )
+    plt.legend()
+    plt.show()
